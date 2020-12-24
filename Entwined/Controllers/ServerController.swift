06fe064ad58663233b1692fbd6e7466b4e20b53c
charles.stigler <charles.stigler@workday.com>
2020-12-24 14:10:31 -0800

    Switch back default server

--- Entwined/Controllers/ServerController.swift
+++ Entwined/Controllers/ServerController.swift
@@ -36,7 +36,7 @@ class ServerController: NSObject, PKJSONSocketDelegate {
             print(connected ? "Connected" : "Disconnected")
         }
     }
-    var serverHostname: String = "10.0.1.140" {
+    var serverHostname: String = "10.0.0.3" {
         didSet {
             disconnect()
             connect()
